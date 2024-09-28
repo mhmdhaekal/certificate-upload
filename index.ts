3f11360571a1c1cb7eb39e144c20897c65dd7d53
Haekal Kalipaksi <hkalipaksi@proton.me>
2024-09-28 23:16:14 +0700

    Update

--- index.ts
+++ index.ts
@@ -3,66 +3,62 @@ import { s3Disk } from "./lib/s3_disk.ts";
 import sql from "./lib/sql.ts";
 
 type Message = {
-  certificateId: string;
+	certificateId: string;
 };
 
 const broker = process.env.BROKER;
-const username = process.env.BROKER_USERNAME;
-const password = process.env.BROKER_PASSWORD;
 
 const kafka = new Kafka({
-  brokers: [`${broker}`],
-  ssl: true,
-  sasl: {
-    mechanism: "scram-sha-256",
-    username: `${username}`,
-    password: `${password}`,
-  },
-  logLevel: logLevel.ERROR,
+	brokers: [`${broker}`],
+	clientId: "certificate-upload",
+	logLevel: logLevel.ERROR,
 });
 
 const consumer = kafka.consumer({ groupId: "uploader-1" });
 
 const run = async () => {
-  await consumer.connect();
-  await consumer.subscribe({
-    topic: "upload-certificate",
-    fromBeginning: true,
-  });
+	await consumer.connect();
+	console.log("Success connect");
+	await consumer.subscribe({
+		topic: `${process.env.TOPIC}`,
+		fromBeginning: true,
+	});
 
-  await consumer.run({
-    eachMessage: async ({ message }) => {
-      if (message.value) {
-        let messageJson = message.value.toString();
-        let uploadMessage = JSON.parse(messageJson) as Message;
-        console.log(`Get message for ${uploadMessage.certificateId}`);
-        let image = await fetch(
-          `http://localhost:8080/certificate/${uploadMessage.certificateId}/image`,
-        );
-        if (image.status !== 200) {
-          throw new Error("Failed to fetch image");
-        }
-        let imageArr = await image.arrayBuffer();
-        let imageFile = Buffer.from(imageArr);
-        let pdf = await fetch(
-          `http://localhost:8080/certificate/${uploadMessage.certificateId}/pdf`,
-        );
-        if (pdf.status !== 200) {
-          throw new Error("Failed to fetch pdf");
-        }
-        let pdfArr = await pdf.arrayBuffer();
-        let pdfFile = Buffer.from(pdfArr);
-        let pdfKey = `certificate/${uploadMessage.certificateId}/${uploadMessage.certificateId}.pdf`;
-        let jpegKey = `certificate/${uploadMessage.certificateId}/${uploadMessage.certificateId}.jpeg`;
-        await s3Disk.put(pdfKey, pdfFile);
-        console.log(`Successfully uploaded pdf file`);
-        await s3Disk.put(jpegKey, imageFile);
-        console.log(`Successfully uploaded jpf file`);
-        await sql`UPDATE generated_certificates SET is_uploaded=true WHERE id=${uploadMessage.certificateId}`;
-        console.log(`Success upload and update ${uploadMessage.certificateId}`);
-      }
-    },
-  });
+	console.log("Success subscribe");
+
+	await consumer.run({
+		eachMessage: async ({ message }) => {
+			if (message.value) {
+				let messageJson = message.value.toString();
+				let uploadMessage = JSON.parse(messageJson) as Message;
+				console.log(`Get message for ${uploadMessage.certificateId}`);
+				let image = await fetch(
+					`http://localhost:3000/api/certificate/${uploadMessage.certificateId}/image`,
+				);
+				if (image.status !== 200) {
+					throw new Error("Failed to fetch image");
+				}
+				let imageArr = await image.arrayBuffer();
+				let imageFile = Buffer.from(imageArr);
+				let pdf = await fetch(
+					`http://localhost:3000/api/certificate/${uploadMessage.certificateId}/pdf`,
+				);
+				if (pdf.status !== 200) {
+					throw new Error("Failed to fetch pdf");
+				}
+				let pdfArr = await pdf.arrayBuffer();
+				let pdfFile = Buffer.from(pdfArr);
+				let pdfKey = `certificate/${uploadMessage.certificateId}/${uploadMessage.certificateId}.pdf`;
+				let jpegKey = `certificate/${uploadMessage.certificateId}/${uploadMessage.certificateId}.jpeg`;
+				await s3Disk.put(pdfKey, pdfFile);
+				console.log(`Successfully uploaded pdf file`);
+				await s3Disk.put(jpegKey, imageFile);
+				console.log(`Successfully uploaded jpf file`);
+				await sql`UPDATE generated_certificates SET is_uploaded=true WHERE id=${uploadMessage.certificateId}`;
+				console.log(`Success upload and update ${uploadMessage.certificateId}`);
+			}
+		},
+	});
 };
 
 run().catch((e) => console.error("[uploader-1/consumer] e.message", e));
